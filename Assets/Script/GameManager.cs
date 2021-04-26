fdb8232f82cbd098d89f432295d8c2e0d5aace0e
specialOne16 <jundi2303@gmail.com>
2021-04-26 14:54:49 +0700

    add game manager and simple movement

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int scoreP1 = 0;
+    [SerializeField] int scoreP2 = 0;
+
+    public GameObject player1;
+    public GameObject player2;
+
+    private PlayerMovement player1Movement;
+    private PlayerMovement player2Movement;
+    private int match = 0;
+
+    private void Start()
+    {
+        player1Movement = player1.GetComponent<PlayerMovement>();
+        player2Movement = player2.GetComponent<PlayerMovement>();
+    }
+
+    public void Tag(string chaser)
+    {
+        Debug.Log($"{chaser} catches!");
+
+        player1Movement.RestartPosition();
+        player2Movement.RestartPosition();
+
+        player1Movement.enabled = false;
+        player2Movement.enabled = false;
+
+        match++;
+
+        StartCoroutine("ResumeMatch");
+
+    }
+
+    IEnumerator ResumeMatch()
+    {
+        yield return new WaitForSeconds(1f);
+
+        player1Movement.enabled = true;
+        player2Movement.enabled = true;
+
+        if(match%2 == 0)
+        {
+            player1.tag = "Chaser";
+            player2.tag = "Runner";
+        }
+        else
+        {
+            player1.tag = "Runner";
+            player2.tag = "Chaser";
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed;
+    public GameManager gameManager;
+
+    private Vector3 initPosition;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        initPosition = transform.position;
+        RestartPosition();
+    }
+
+    private void Update()
+    {
+        transform.position = transform.position + velocity * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.CompareTag("Chaser"))
+        {
+            if (collision.collider.CompareTag("Runner"))
+            {
+                gameManager.Tag(gameObject.name);
+            }
+        }
+    }
+
+    public void RestartPosition()
+    {
+        transform.position = initPosition;
+        velocity = Vector3.right * speed;
+    }
+}
