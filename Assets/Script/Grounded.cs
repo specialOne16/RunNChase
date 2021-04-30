315c4e87c77a328f03d01f7f11ea00729abdb174
Lionnarta <lionnarta.savirandy2000@gmail.com>
2021-04-30 16:09:31 +0700

    simple player movement, player jump, and sample scene

---
+++ Assets/Script/Grounded.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            player.GetComponent<PlayerMovement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            player.GetComponent<PlayerMovement>().isGrounded = false;
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int speed;
+    public float speed = 10;
+    public float acceleration = 15;
+    public float jumpForce = 5;
     public GameManager gameManager;
+    public bool isGrounded = false;
 
     private Vector3 initPosition;
-    private Vector3 velocity;
+    private Rigidbody2D rigidbody2D;
 
     void Start()
     {
         initPosition = transform.position;
+        rigidbody2D = GetComponent<Rigidbody2D>();
         RestartPosition();
     }
 
     private void Update()
     {
-        transform.position = transform.position + velocity * Time.deltaTime;
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -35,6 +41,34 @@ public class PlayerMovement : MonoBehaviour
     public void RestartPosition()
     {
         transform.position = initPosition;
-        velocity = Vector3.right * speed;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private float IncreaseSpeedTo(float n, float target, float acc)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+        else
+        {
+            float direction = Mathf.Sign(target - n);
+            n += acc * Time.deltaTime * direction;
+            if (direction == Mathf.Sign(target - n))
+            {
+                return n;
+            }
+            else
+            {
+                return target;
+            }
+        }
     }
 }
